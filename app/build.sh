65e8debf8377286f26ec044329d74e6274e1fe72
suyf <2499522170@qq.com>
2022-12-31 15:54:15 +0800

    shell脚本构建之前判断sdk获取是否ok

--- app/build.sh
+++ app/build.sh
@@ -9,6 +9,7 @@ declare -a arguments # -a声明list
 declare androidSdk
 declare buildType
 declare lastGitVer
+declare newPkgName
 
 function handleOptions() {
   processOptions $*
@@ -17,22 +18,30 @@ function handleOptions() {
     buildType="debug"
   fi
   echo "buildType==$buildType"
+
+  newPkgName=${options["newPkgName"]}
+  if [ "$newPkgName" == "DEFAULT" ]; then
+    newPkgName="com.suyf.test.def"
+  fi
+  echo "newPkgName==$newPkgName"
 }
 
 function handleAndroidSdk() {
   processAndroidSdk $*
-  echo "Android SDK Path=$androidSdk"
-  export PATH=$androidSdk/tools:$androidSdk/platform-tools:$PATH
-  export LANG=en_US.UTF-8
+  if [ -n "$androidSdk" ]; then
+    echo "Android SDK Path=$androidSdk"
+    export PATH=$androidSdk/tools:$androidSdk/platform-tools:$PATH
+    export LANG=en_US.UTF-8
+  else
+    echo "Android SDK Path is empty"
+  fi
 }
 
-function preProcessBuild() {
+function beforeProcessBuild() {
   if [ -z $lastGitVer ]; then
     lastGitVer=$(git log --pretty=format:'%h' -n 1)
   fi
   echo "lastGitVer=$lastGitVer"
-
-  echo "preProcessBuild"
 }
 
 function startGradleBuild() {
@@ -48,7 +57,7 @@ function startGradleBuild() {
   #-i/--info -d/--debug -s/--stacktrace
   #-P传递构建参数给project ext，可以通过project.ext.get("newPkgName")获取
   bash ../gradlew ":app:assemble${buildType}" -s \
-    -PnewPkgName=${options["newPkgName"]} \
+    -PnewPkgName=${newPkgName} \
     2>&1 | tee -a buildLog && buildOk="true"
 
   [ -d bin ] || mkdir -p bin
@@ -61,6 +70,26 @@ function startGradleBuild() {
   fi
 }
 
+function afterProcessBuild() {
+  if ! [ -z $lastGitVer ]; then
+    git reset --hard $lastGitVer
+  fi
+}
+
+function testShellCall() {
+  #test isOptionEnabled call
+  if isOptionEnabled "push"; then
+    echo "is push enable=true"
+  else
+    echo "is push enable=false"
+  fi
+  #打开Terminal，cd app切换到app目录下，sudo chmod +x build.sh添加可执行权限
+  #./build.sh --buildType=debug --newPkgName="com.suyf.test" --channel=yyb --enable-push
+  #./build.sh --buildType=release --newPkgName="com.suyf.test" --channel=yyb --enable-push
+  echo "options-01:${options["newPkgName"]}"
+  echo "options-02:${options["channel"]}"
+}
+
 function main() {
   source ./options.sh
 
@@ -68,25 +97,14 @@ function main() {
   local buildOk=false
   handleOptions "$@"
   handleAndroidSdk "$@"
-  preProcessBuild
+  if [ -z $androidSdk ]; then
+    echo "Build FAILURE!!!"
+    return 1
+  fi
+  beforeProcessBuild
   startGradleBuild && buildOk=true
-
-  #test isOptionEnabled call
-  if isOptionEnabled "push"; then
-    echo "is push enable=true"
-  else
-    echo "is push enable=false"
-  fi
-
-  #打开Terminal，cd app切换到app目录下，sudo chmod +x build.sh添加可执行权限
-  #./build.sh --buildType=debug --newPkgName="com.suyf.test" --channel=yyb --enable-push
-  #./build.sh --buildType=release --newPkgName="com.suyf.test" --channel=yyb --enable-push
-  echo "options-01:${options["newPkgName"]}"
-  echo "options-02:${options["channel"]}"
-
-  if ! [ -z $lastGitVer ]; then
-    git reset --hard $lastGitVer
-  fi
+  afterProcessBuild
+  testShellCall
 
   if [ $buildOk = true ]; then
     echo "Build SUCCESS"
